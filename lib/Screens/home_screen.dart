38a82d34397f4dc370203a1f1f155c643e22d0e6
southpole01 <karankunwar59@gmail.com>
2021-04-08 20:20:46 +0530

    Local Changes

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:madboxes/Auth/authentication.dart';
 import 'package:madboxes/Auth/sign_in.dart';
 import 'package:madboxes/Utils/theme.dart';
@@ -62,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
       },
     );
     Timer.periodic(
-        Duration(seconds: 1), (Timer t) => _getCurrentForegroundApp());
+        Duration(seconds: 10), (Timer t) => _getCurrentForegroundApp());
   }
 
   @override
@@ -85,151 +86,182 @@ class _HomeScreenState extends State<HomeScreen> {
         },
       );
     }
-
+    var scaffoldKey = GlobalKey<ScaffoldState>();
     maxHeight = MediaQuery.of(context).size.height;
     maxWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Consumer<ThemeNotifier>(
-                    builder: (context, notifier, child) => IconButton(
-                        icon: notifier.isDarkTheme
-                            ? FaIcon(
-                                FontAwesomeIcons.moon,
-                                size: 20,
-                                color: Colors.white,
-                              )
-                            : Icon(Icons.wb_sunny),
-                        onPressed: () => {notifier.toggleTheme()})),
-                Consumer<ThemeNotifier>(
+      key: scaffoldKey,
+      drawer: Drawer(
+        child: Container(
+          color: Theme.of(context).primaryColor,
+          child: ListView(
+            children: [
+              Consumer<ThemeNotifier>(
                   builder: (context, notifier, child) => IconButton(
                       icon: notifier.isDarkTheme
-                          ? Icon(
-                              Icons.exit_to_app_rounded,
-                              size: 20,
-                              color: Colors.white,
-                            )
-                          : Icon(Icons.exit_to_app_rounded),
-                      onPressed: () async {
-                        setState(() {
-                          _isSigningOut = true;
-                        });
-                        await Authentication.signOut(context: context);
-                        setState(() {
-                          _isSigningOut = false;
-                        });
-                        Navigator.of(context)
-                            .pushReplacement(_routeToSignInScreen());
-                      }),
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.black54,
-              padding: EdgeInsets.only(top: 25, left: 20, right: 25),
-              height: maxHeight / 5,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(widget.user.displayName,style: TextStyle(color: Colors.blue,fontSize: 26,fontWeight: FontWeight.bold)),
-                      Text("Software Developer",style: TextStyle(color: Colors.white,fontSize: 18)),
-                      SizedBox(height: 20,),
-                      Text("Stats..",style: TextStyle(color: Colors.white,fontSize: 37,fontWeight: FontWeight.w900)),
-                  ],),
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundImage: NetworkImage(widget.user.photoURL),
-                  ),
-                ],
+                          ? FaIcon(
+                        FontAwesomeIcons.moon,
+                        size: 20,
+                        color: notifier.isDarkTheme? Colors.white:Colors.black54,
+                      )
+                          : Icon(Icons.wb_sunny),
+                      onPressed: () => {notifier.toggleTheme()})),
+              Consumer<ThemeNotifier>(
+                builder: (context, notifier, child) => IconButton(
+                    icon: notifier.isDarkTheme
+                        ? Icon(
+                      Icons.exit_to_app_rounded,
+                      size: 20,
+                      color: notifier.isDarkTheme? Colors.white:Colors.black54,
+                    )
+                        : Icon(Icons.exit_to_app_rounded),
+                    onPressed: () async {
+                      setState(() {
+                        _isSigningOut = true;
+                      });
+                      await Authentication.signOut(context: context);
+                      setState(() {
+                        _isSigningOut = false;
+                      });
+                      Navigator.of(context)
+                          .pushReplacement(_routeToSignInScreen());
+                    }),
               ),
-            ),
-            Container(
-              color: Theme.of(context).primaryColor,
-              height: maxHeight * 4 / 5 - 100,
-              child: Padding(
-                padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-                child: ListView(
-                  scrollDirection: Axis.vertical,
-                  children: <Widget>[
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-
-                      height: 150,
-                      child: _riveArtboard == null
-                          ? const SizedBox()
-                          : Rive(
-                              artboard: _riveArtboard,
-                              fit: BoxFit.fill,
-                            ),
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              width: maxWidth/2-15,
-                              height: 150,
-                            ),
-                            SizedBox(height: 10,width: maxWidth/2-30,),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.lightGreenAccent,
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              width: maxWidth/2-15,
-                              height: 150,
-                            ),
-                          ],
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          width: maxWidth/2-15,
-                          height: 310,
-                          // child: ElevatedButton(
-                          //   onPressed: () {
-                          //
-                          //   },
-                          // ),
-                        ),
-
-                      ],
-                    ),
-                    SizedBox(height: 10,),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlueAccent,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      height: 150,
-                    ),
-                    SizedBox(height: 50,),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
+      ),
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+            children: [
+              SizedBox(height: 10,),
+              Container(
+                color: Colors.black54,
+                padding: EdgeInsets.only(top: 25, left: 20, right: 25),
+                height: maxHeight / 4,
+                child: Column(
+                  children: [
+                    SizedBox(height:20),
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20,),
+                          Text(widget.user.displayName,style: GoogleFonts.montserrat(color: Colors.blue, fontSize: 16)),
+                          Text("Software Developer",style: TextStyle(color: Colors.white,fontSize: 18)),
+                          SizedBox(height: 20,),
+                          Text("Stats..",style: TextStyle(color: Colors.white,fontSize: 37,fontWeight: FontWeight.w900)),
+                      ],),
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundImage: NetworkImage(widget.user.photoURL),
+                      ),
+                    ],
+                  ),
+                  ]
+                ),
+              ),
+              Container(
+                color: Theme.of(context).primaryColor,
+                height: maxHeight * 4 / 5-60,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+
+                        height: 150,
+                        child: _riveArtboard == null
+                            ? const SizedBox()
+                            : Rive(
+                                artboard: _riveArtboard,
+                                fit: BoxFit.fill,
+                              ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: Column(
+                                    children: [
+                                      Text("Set",),
+                                      Text("Schedule",),
+                                    ],
+                                  ),
+                                ),
+                                width: maxWidth/2-15,
+                                height: 150,
+                              ),
+                              SizedBox(height: 10,width: maxWidth/2-30,),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.lightGreenAccent,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                width: maxWidth/2-15,
+                                height: 150,
+                              ),
+                            ],
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            width: maxWidth/2-15,
+                            height: 310,
+                            // child: ElevatedButton(
+                            //   onPressed: () {
+                            //
+                            //   },
+                            // ),
+                          ),
+
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlueAccent,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 150,
+                      ),
+                      SizedBox(height: 50,),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+        ),
+          ),
+          Positioned(
+            left: 10,
+            top: 20,
+            child: IconButton(
+              icon: Icon(Icons.menu,color: Colors.white,),
+              onPressed: () => scaffoldKey.currentState.openDrawer(),
+            ),
+          ),
+        ],
       ),
     );
   }
